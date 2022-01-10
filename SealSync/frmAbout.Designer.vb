bc98821ace5b2dff48bc79bac7cf2b84f458f985
Seeloewen <Louis.Herbert2006@gmail.com>
2022-01-10 20:44:25 +0100

    Added changelog and fixed typos

--- SealSync/frmAbout.Designer.vb
+++ SealSync/frmAbout.Designer.vb
@@ -29,7 +29,14 @@ Partial Class frmAbout
         Me.gbLicense = New System.Windows.Forms.GroupBox()
         Me.rbtLicense = New System.Windows.Forms.RichTextBox()
         Me.btnOK = New System.Windows.Forms.Button()
+        Me.tcAbout = New System.Windows.Forms.TabControl()
+        Me.tpLicense = New System.Windows.Forms.TabPage()
+        Me.tpChangelog = New System.Windows.Forms.TabPage()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
         Me.gbLicense.SuspendLayout()
+        Me.tcAbout.SuspendLayout()
+        Me.tpLicense.SuspendLayout()
+        Me.tpChangelog.SuspendLayout()
         Me.SuspendLayout()
         '
         'lblVersion
@@ -40,7 +47,7 @@ Partial Class frmAbout
         Me.lblVersion.Name = "lblVersion"
         Me.lblVersion.Size = New System.Drawing.Size(371, 29)
         Me.lblVersion.TabIndex = 0
-        Me.lblVersion.Text = "SealSync 0.1-b Build 2201062341"
+        Me.lblVersion.Text = "SealSync 0.2-b Build 2201101617"
         '
         'lblDescription
         '
@@ -48,15 +55,15 @@ Partial Class frmAbout
         Me.lblDescription.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblDescription.Location = New System.Drawing.Point(14, 42)
         Me.lblDescription.Name = "lblDescription"
-        Me.lblDescription.Size = New System.Drawing.Size(372, 20)
+        Me.lblDescription.Size = New System.Drawing.Size(381, 20)
         Me.lblDescription.TabIndex = 1
-        Me.lblDescription.Text = "Simple folder syncronization program by Seeloewen" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.lblDescription.Text = "Simple folder synchronization program by Seeloewen" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
         'llblGithub
         '
         Me.llblGithub.AutoSize = True
         Me.llblGithub.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.llblGithub.Location = New System.Drawing.Point(20, 306)
+        Me.llblGithub.Location = New System.Drawing.Point(18, 428)
         Me.llblGithub.Name = "llblGithub"
         Me.llblGithub.Size = New System.Drawing.Size(137, 20)
         Me.llblGithub.TabIndex = 2
@@ -67,9 +74,9 @@ Partial Class frmAbout
         '
         Me.gbLicense.Controls.Add(Me.rbtLicense)
         Me.gbLicense.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.gbLicense.Location = New System.Drawing.Point(18, 77)
+        Me.gbLicense.Location = New System.Drawing.Point(6, 6)
         Me.gbLicense.Name = "gbLicense"
-        Me.gbLicense.Size = New System.Drawing.Size(366, 221)
+        Me.gbLicense.Size = New System.Drawing.Size(403, 308)
         Me.gbLicense.TabIndex = 3
         Me.gbLicense.TabStop = False
         Me.gbLicense.Text = "GNU GPL v3"
@@ -79,29 +86,72 @@ Partial Class frmAbout
         Me.rbtLicense.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.rbtLicense.Location = New System.Drawing.Point(6, 21)
         Me.rbtLicense.Name = "rbtLicense"
-        Me.rbtLicense.Size = New System.Drawing.Size(354, 193)
+        Me.rbtLicense.Size = New System.Drawing.Size(391, 281)
         Me.rbtLicense.TabIndex = 4
         Me.rbtLicense.Text = resources.GetString("rbtLicense.Text")
         '
         'btnOK
         '
         Me.btnOK.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnOK.Location = New System.Drawing.Point(299, 304)
+        Me.btnOK.Location = New System.Drawing.Point(354, 426)
         Me.btnOK.Name = "btnOK"
         Me.btnOK.Size = New System.Drawing.Size(85, 26)
         Me.btnOK.TabIndex = 4
         Me.btnOK.Text = "OK"
         Me.btnOK.UseVisualStyleBackColor = True
         '
+        'tcAbout
+        '
+        Me.tcAbout.Controls.Add(Me.tpLicense)
+        Me.tcAbout.Controls.Add(Me.tpChangelog)
+        Me.tcAbout.Location = New System.Drawing.Point(18, 74)
+        Me.tcAbout.Name = "tcAbout"
+        Me.tcAbout.SelectedIndex = 0
+        Me.tcAbout.Size = New System.Drawing.Size(421, 346)
+        Me.tcAbout.TabIndex = 5
+        '
+        'tpLicense
+        '
+        Me.tpLicense.BackColor = System.Drawing.Color.White
+        Me.tpLicense.Controls.Add(Me.gbLicense)
+        Me.tpLicense.Location = New System.Drawing.Point(4, 22)
+        Me.tpLicense.Name = "tpLicense"
+        Me.tpLicense.Padding = New System.Windows.Forms.Padding(3)
+        Me.tpLicense.Size = New System.Drawing.Size(413, 320)
+        Me.tpLicense.TabIndex = 0
+        Me.tpLicense.Text = "License"
+        '
+        'tpChangelog
+        '
+        Me.tpChangelog.BackColor = System.Drawing.Color.White
+        Me.tpChangelog.Controls.Add(Me.RichTextBox1)
+        Me.tpChangelog.Location = New System.Drawing.Point(4, 22)
+        Me.tpChangelog.Name = "tpChangelog"
+        Me.tpChangelog.Padding = New System.Windows.Forms.Padding(3)
+        Me.tpChangelog.Size = New System.Drawing.Size(413, 320)
+        Me.tpChangelog.TabIndex = 1
+        Me.tpChangelog.Text = "Changelog"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.Color.White
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(7, 7)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(400, 307)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = resources.GetString("RichTextBox1.Text")
+        '
         'frmAbout
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(404, 341)
-        Me.Controls.Add(Me.btnOK)
-        Me.Controls.Add(Me.gbLicense)
+        Me.ClientSize = New System.Drawing.Size(453, 463)
         Me.Controls.Add(Me.llblGithub)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.tcAbout)
         Me.Controls.Add(Me.lblDescription)
         Me.Controls.Add(Me.lblVersion)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
@@ -112,6 +162,9 @@ Partial Class frmAbout
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "About"
         Me.gbLicense.ResumeLayout(False)
+        Me.tcAbout.ResumeLayout(False)
+        Me.tpLicense.ResumeLayout(False)
+        Me.tpChangelog.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -123,4 +176,8 @@ Partial Class frmAbout
     Friend WithEvents gbLicense As GroupBox
     Friend WithEvents rbtLicense As RichTextBox
     Friend WithEvents btnOK As Button
+    Friend WithEvents tcAbout As TabControl
+    Friend WithEvents tpLicense As TabPage
+    Friend WithEvents tpChangelog As TabPage
+    Friend WithEvents RichTextBox1 As RichTextBox
 End Class
